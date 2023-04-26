71a9a18e3cbdd061a4071e91273416f7a24275f6
Teiger212 <eyalteiger@gmail.com>
2023-04-26 17:28:39 -0500

    debugging

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -12,6 +12,8 @@ export const fetchPageInfo = async () => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_SANITY_BASE_URL}/api/getPageInfo`
   )
+
+  console.log('result', res)
   const data = await res.json()
   const { pageInfo }: Data = data
 
